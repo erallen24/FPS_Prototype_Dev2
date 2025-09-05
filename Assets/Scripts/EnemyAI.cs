2fba30db026620ba699df75a5fa9c8ed967bd733
drew64789 <adstoever@gmail.com>
2025-09-04 21:44:07 -0400

    Added barrel "Enemy"

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour, IDamage
+{
+    [SerializeField] Renderer model;
+    [SerializeField] int HP;
+    [SerializeField] Color colorFlash;
+
+    Color colorOrig;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        colorOrig = model.material.color;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void takeDamage(int damage)
+    {
+        if (HP > 0) {
+            HP -= damage;
+            StartCoroutine(flash());
+        }
+        if (HP <= 0 ) { Destroy(gameObject); }
+        
+    }
+
+    IEnumerator flash()
+    {
+        model.material.color = colorFlash;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
+    }
+}
