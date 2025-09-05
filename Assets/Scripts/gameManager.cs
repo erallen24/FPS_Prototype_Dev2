012cdbb4c2c1453d76dc540f8ae270e7fe76d891
QuantumTutor <djtwen23@student.fullsail.edu>
2025-09-05 05:45:48 -0400

    Rename gameManager MonoBehaviour to GameManager..AGAIN!

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -8,7 +8,6 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-
     [SerializeField] GameObject menuActive;
     [SerializeField] GameObject subMenuActive;
     [SerializeField] GameObject menuPause;
