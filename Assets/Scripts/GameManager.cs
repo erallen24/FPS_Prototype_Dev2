a2b64586f44f0b426812545f34bb85583ac978cc
OctoStark <robantsch32@gmail.com>
2025-09-08 00:02:53 -0400

    made the win menu show up

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -135,10 +135,10 @@ public class GameManager : MonoBehaviour
     {
         gameGoalCount += amount;
         gameGoalCountText.text = gameGoalCount.ToString("F0");
-        gameGoalMenuStat.text = gameGoalCount.ToString("F0");
+        //gameGoalMenuStat.text = gameGoalCount.ToString("F0");
 
-        Debug.Log("Game Goal Count: " + gameGoalCount);
-        if (gameGoalCount <= 0 && menuActive == null)
+        //Debug.Log("Game Goal Count: " + gameGoalCount);
+        if (gameGoalCount <= 0/* && menuActive == null*/)
         {
             statePause();
             menuActive = menuWin;
