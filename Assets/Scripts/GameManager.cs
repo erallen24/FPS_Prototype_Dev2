faea1a1534a97c8577d21aec9009e5d04ef5d442
OctoStark <robantsch32@gmail.com>
2025-09-08 00:10:10 -0400

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -135,7 +135,7 @@ public class GameManager : MonoBehaviour
     {
         gameGoalCount += amount;
         gameGoalCountText.text = gameGoalCount.ToString("F0");
-        //gameGoalMenuStat.text = gameGoalCount.ToString("F0");
+        //gameGoalMenuStat.text = gameGoalCount.ToString("F0"); //this line was making the win menu not show up
 
         //Debug.Log("Game Goal Count: " + gameGoalCount);
         if (gameGoalCount <= 0/* && menuActive == null*/)
