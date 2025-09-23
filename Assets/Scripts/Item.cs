46db91030fb1030779cdfaa195cff976b7fdc543
OctoStark <robantsch32@gmail.com>
2025-09-22 23:36:59 -0400

    attemped to make the inventory fillable

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Item : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string itemName;
+    [SerializeField] private Sprite itemSprite;
+    [SerializeField] inventoryItem item;
+
+    private InventoryManager inventoryManager;
+
+
+   public float rotateSpeed = 50f; // Speed at which the pickup rotates for visibility
+    public float pulseSpeed = 2f; // Speed of the pulsing effect
+    public float pulseMagnitude = 0.1f; // Magnitude of the pulsing effect
+    private float pulseTimer = 0f; // Timer for pulsing effect
+
+
+    private Vector3 originalPosition;
+    private Vector3 initialScale; // Initial scale of the pickup for pulsing effect
+    private Vector3 initialRotation;
+    private Quaternion rotation;
+
+    private void Start()
+    {
+        originalPosition = transform.position;
+        initialScale = transform.localScale;
+        initialRotation = transform.eulerAngles;
+        rotation = Quaternion.Euler(initialRotation);
+    }
+
+    private void Update()
+    {
+        // Optional: Add any rotation or animation to the pickup object for visual effect
+        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime); // Rotate around the Y-axis
+        // Pulsing effect with up and down movement
+        pulseTimer += Time.deltaTime * pulseSpeed;
+        float scaleFactor = 1 + Mathf.Sin(pulseTimer) * pulseMagnitude;
+        transform.localScale = initialScale * scaleFactor;
+        transform.position = originalPosition + new Vector3(0, Mathf.Sin(pulseTimer) * pulseMagnitude, 0); // Adjust the Y position for pulsing effect
+
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            //IInteractable interactable = other.GetComponent<IInteractable>();
+
+            HUDManager.instance.UpdateInteractPrompt("Press 'E' to pick up " + item.itemName);
+            HUDManager.instance.interactPromptText.color = Color.white;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        HUDManager.instance.UpdateInteractPrompt("");
+    }
+
+    public void Interact()
+    {
+        Debug.Log("Should be picking up");
+        GameManager.instance.playerScript.AddItem(item);
+
+        Destroy(gameObject);
+        HUDManager.instance.UpdateInteractPrompt("");
+    }
+}
