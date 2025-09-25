5bfe71e6640c6c4a78344da7f6a9b87b708a6f12
Infin808 <calebsorenson8@gmail.com>
2025-09-24 23:23:39 -0600

    Update to the skill tree tab in the menu
    - Added descriptions to each perk point
    - Fixed perk acquisition process

---
+++ Assets/Scripts/perkNode.cs
@@ -0,0 +1,43 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class perkNode : MonoBehaviour
+{
+    public GameObject textBox;
+    public GameObject nextAcquireButton;
+
+    public Sprite availableNode;
+    public Sprite unavailableNode;
+    public Sprite purchasedNode;
+
+    public Image currImage;
+    public Image nextImage;
+
+
+
+    private bool descripActive;
+
+
+    public void showDescription()
+    {
+        descripActive = !descripActive;
+
+        textBox.SetActive(descripActive);
+    }
+
+    public void purchaseSkill()
+    {
+        currImage.sprite = purchasedNode;
+        currImage.rectTransform.sizeDelta = new Vector2(200, 200);
+
+        if (nextImage.sprite != purchasedNode)
+        {
+            nextImage.sprite = availableNode;
+        }
+
+        nextAcquireButton.SetActive(true);
+
+    }
+
+}
